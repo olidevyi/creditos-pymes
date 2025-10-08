0b6a3b85f4bfa92eecc0c3a6f916da4232dfaadb
Oliver Yantas <jean.yantas@gmail.com>
2025-10-07 20:43:38 -0500

    Packages basic creation

---
+++ BackEnd/src/test/java/com/setiembre2025nocountry/creditospymes/backend/BackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.setiembre2025nocountry.creditospymes.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
